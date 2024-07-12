c378f43fe236bbc15d9710a0f3a22ef1ca86671c
Tobi Oloke <tobzdaman619@gmail.com>
2024-07-12 09:51:21 +0100

    Pointing to the new tag

--- mediasynclite.nix
+++ mediasynclite.nix
@@ -13,26 +13,26 @@
 
 stdenv.mkDerivation rec {
   pname = "mediasynclite";
-  version = "0.4.2";
+  version = "0.4.3";
 
   src = fetchFromGitHub {
     owner = "iBroadcastMediaServices";
     repo = "MediaSyncLiteLinux";
     rev = version;
-    hash = "sha256-ToSkR6tPJMBCcj1NUBAywKjCAPlpmh+ngIopFrT2PIA=";
+    hash = "sha256-KIXt7fZ6WAy2z704OovmTU47meZYQenNIUY2/JnY83w=";
     };
 
   buildInputs = [
    curl
-   glib
-   gtk3
-   openssl
+   openssl.dev
    jansson
   ];
 
   strictDeps = true;
 
   nativeBuildInputs = [
+   gtk3
+   glib
    gsettings-desktop-schemas
    pkg-config
    wrapGAppsHook3
@@ -46,7 +46,7 @@ stdenv.mkDerivation rec {
 
   meta = with lib; {
     description = "A Linux-native graphical uploader for iBroadcast";
-    downloadPage = "https://project.ibroadcast.com/";
+    downloadPage = "https://github.com/tobz619/MediaSyncLiteLinuxNix";
     homepage = "https://github.com/iBroadcastMediaServices/MediaSyncLiteLinux";
     license = licenses.gpl3Only;
     maintainers = with maintainers; [ tobz619 ];
